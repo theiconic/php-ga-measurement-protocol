1946c31ded4c17e8f830a075ebf4064eb35bc10d
Jorge A. Borges <contact@jorgeborges.me>
2014-09-23 00:17:58 +1000

    Adding classes for Hit parameters

--- src/Parameters/Hit/HitType.php
+++ src/Parameters/Hit/HitType.php
@@ -8,5 +8,19 @@ class HitType extends SingleParameter
 {
     const HIT_TYPE_PAGEVIEW = 'pageview';
 
+    const HIT_TYPE_EVENT = 'event';
+
+    const HIT_TYPE_SCREENVIEW = 'screenview';
+
+    const HIT_TYPE_TRANSACTION = 'transaction';
+
+    const HIT_TYPE_ITEM = 'item';
+
+    const HIT_TYPE_SOCIAL = 'social';
+
+    const HIT_TYPE_EXCEPTION = 'exception';
+
+    const HIT_TYPE_TIMING = 'timing';
+
     protected $name = 't';
 }
---
+++ src/Parameters/Hit/NonInteractionHit.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TheIconic\Tracking\GoogleAnalytics\Parameters\Hit;
+
+use TheIconic\Tracking\GoogleAnalytics\Parameters\SingleParameter;
+
+class NonInteractionHit extends SingleParameter
+{
+    protected $name = 'ni';
+}
