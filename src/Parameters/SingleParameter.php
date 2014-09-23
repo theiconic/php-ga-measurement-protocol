f40a0705c0057cfcb8fc5f093045df2b21ca0652
Jorge A. Borges <contact@jorgeborges.me>
2014-09-23 18:41:28 +1000

    Documenting SingleParameter class

--- src/Parameters/SingleParameter.php
+++ src/Parameters/SingleParameter.php
@@ -2,12 +2,33 @@
 
 namespace TheIconic\Tracking\GoogleAnalytics\Parameters;
 
+/**
+ * Class SingleParameter
+ *
+ * A parameter is composed of a name and a value.
+ *
+ * @package TheIconic\Tracking\GoogleAnalytics\Parameters
+ */
 abstract class SingleParameter
 {
+    /**
+     * Name for a parameter in GA Measurement Protocol.
+     * Gets send as the name for a query parameter in the HTTP request.
+     *
+     * @var string
+     */
     protected $name = '';
 
+    /**
+     * Value for the parameter.
+     *
+     * @var mixed
+     */
     protected $value = null;
 
+    /**
+     * Constructor. Validates that the child class has declared a non empty name for the parameter.
+     */
     public function __construct()
     {
         if (empty($this->name)) {
@@ -15,11 +36,22 @@ abstract class SingleParameter
         }
     }
 
+    /**
+     * Gets the name for the parameter.
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Sets a value for the parameter.
+     *
+     * @param $value
+     * @return $this
+     */
     public function setValue($value)
     {
         $this->value = $value;
@@ -27,6 +59,11 @@ abstract class SingleParameter
         return $this;
     }
 
+    /**
+     * Gets the value for the parameter.
+     *
+     * @return mixed
+     */
     public function getValue()
     {
         return $this->value;
