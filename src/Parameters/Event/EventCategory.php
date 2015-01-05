4071bbcfb5f7177843dccaf98b54a32cc4f70f43
Jorge A. Borges <contact@jorgeborges.me>
2015-01-05 20:43:34 +1100

    Documentation for Event category parameters

--- src/Parameters/Event/EventCategory.php
+++ src/Parameters/Event/EventCategory.php
@@ -4,7 +4,19 @@ namespace TheIconic\Tracking\GoogleAnalytics\Parameters\Event;
 
 use TheIconic\Tracking\GoogleAnalytics\Parameters\SingleParameter;
 
+/**
+ * Class EventCategory
+ *
+ * @link https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#ec
+ *
+ * @package TheIconic\Tracking\GoogleAnalytics\Parameters\Event
+ */
 class EventCategory extends SingleParameter
 {
+    /**
+     * @inheritDoc
+     *
+     * @var string
+     */
     protected $name = 'ec';
 }
