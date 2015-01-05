4baf240fe94390b44037051e725dfafbed994990
Jorge A. Borges <contact@jorgeborges.me>
2015-01-05 20:26:00 +1100

    Applying PSR-4 style

--- src/Parameters/CompoundParameter.php
+++ src/Parameters/CompoundParameter.php
@@ -19,13 +19,15 @@ abstract class CompoundParameter
             $matchExists = false;
             foreach ($this->parameterNameMapper as $regex => $parameterName) {
                 if (preg_match($regex, $name, $matches) === 1) {
-	                $dynamicName = '';
-	                if(isset($matches[1]))
-	                {
-		                $dynamicName = $matches[1];
-	                }
+                    $dynamicName = '';
+
+                    if (isset($matches[1])) {
+                        $dynamicName = $matches[1];
+                    }
+
                     $matchExists = true;
                     $this->parameters[$parameterName . $dynamicName] = $value;
+
                     break;
                 }
             }
