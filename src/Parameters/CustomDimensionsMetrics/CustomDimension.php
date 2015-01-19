216cae265519e2772175e4b90c677d9eb82bc04c
Jorge A. Borges <contact@jorgeborges.me>
2015-01-19 22:22:04 +1100

    Adding Custom Dimension / Metrics category indexed parameters

---
+++ src/Parameters/CustomDimensionsMetrics/CustomDimension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TheIconic\Tracking\GoogleAnalytics\Parameters\CustomDimensionsMetrics;
+
+use TheIconic\Tracking\GoogleAnalytics\Parameters\SingleParameter;
+
+/**
+ * Class CustomDimension
+ *
+ * @link https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#cd_
+ *
+ * @package TheIconic\Tracking\GoogleAnalytics\Parameters\CustomDimensionsMetrics
+ */
+class CustomDimension extends SingleParameter
+{
+    /**
+     * @inheritDoc
+     *
+     * @var string
+     */
+    protected $name = 'cd';
+
+    /**
+     * @inheritDoc
+     *
+     * @var bool
+     */
+    protected $isIndexed = true;
+}
---
+++ src/Parameters/CustomDimensionsMetrics/CustomMetric.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TheIconic\Tracking\GoogleAnalytics\Parameters\CustomDimensionsMetrics;
+
+use TheIconic\Tracking\GoogleAnalytics\Parameters\SingleParameter;
+
+/**
+ * Class CustomMetric
+ *
+ * @link https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#cm_
+ *
+ * @package TheIconic\Tracking\GoogleAnalytics\Parameters\CustomDimensionsMetrics
+ */
+class CustomMetric extends SingleParameter
+{
+    /**
+     * @inheritDoc
+     *
+     * @var string
+     */
+    protected $name = 'cm';
+
+    /**
+     * @inheritDoc
+     *
+     * @var bool
+     */
+    protected $isIndexed = true;
+}
