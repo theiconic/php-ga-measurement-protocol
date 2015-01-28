9fb2abf1bc92e652bbc426f74469818fac6745d0
Jorge A. Borges <contact@jorgeborges.me>
2015-01-28 13:43:22 +1100

    Adding testing class

---
+++ tests/support/classes/SingleTestParameterIndexed.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TheIconic\Tracking\GoogleAnalytics\Tests;
+
+use TheIconic\Tracking\GoogleAnalytics\Parameters\SingleParameter;
+
+class SingleTestParameterIndexed extends SingleParameter
+{
+    protected $name = 'testi';
+
+    protected $isIndexed = true;
+}
