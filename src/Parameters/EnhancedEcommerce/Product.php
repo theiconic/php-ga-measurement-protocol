dedea1db9f4ec696944472ed2027f8a790fbc142
Jorge A. Borges <contact@jorgeborges.me>
2015-01-29 19:11:58 +1100

    Unit tests for CompoundParameter and declaring required fields for Product

--- src/Parameters/EnhancedEcommerce/Product.php
+++ src/Parameters/EnhancedEcommerce/Product.php
@@ -34,20 +34,5 @@ class Product extends CompoundParameter
         '/^custom_metric_(\d{1,3})$/' => 'cm',
     ];
 
-    /**
-     * Validates the product. SKU and name are required.
-     * Translates and saves the product fields to be sent later in case they are valid.
-     *
-     * @throws \InvalidArgumentException
-     *
-     * @param array $productData
-     */
-    public function __construct(array $productData)
-    {
-        if (empty($productData['sku']) && empty($productData['name'])) {
-            throw new \InvalidArgumentException('Either SKU or Name must be set for a product');
-        }
-
-        $this->saveCompoundParameterData($productData);
-    }
+    protected $requiredParameters = ['sku', 'name'];
 }
