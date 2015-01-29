a14e21568b26f6557b10904599ac1c56751df668
Jorge A. Borges <contact@jorgeborges.me>
2015-01-29 19:07:34 +1100

    Adding docs for CompoundParameter

--- src/Parameters/CompoundParameter.php
+++ src/Parameters/CompoundParameter.php
@@ -2,17 +2,56 @@
 
 namespace TheIconic\Tracking\GoogleAnalytics\Parameters;
 
+use TheIconic\Tracking\GoogleAnalytics\Exception\InvalidCompoundParameterException;
+
+/**
+ * Class CompoundParameter
+ *
+ * @package TheIconic\Tracking\GoogleAnalytics\Parameters
+ */
 abstract class CompoundParameter
 {
+    /**
+     * @var array
+     */
     protected $parameterNameMapper = [];
 
+    /**
+     * @var array
+     */
+    protected $requiredParameters = [];
+
+    /**
+     * @var array
+     */
     protected $parameters = [];
 
+
+    public function __construct(array $compoundData)
+    {
+        foreach ($this->requiredParameters as $requiredParameter) {
+            if (!array_key_exists($requiredParameter, $compoundData)) {
+                throw new InvalidCompoundParameterException(
+                    $requiredParameter . ' is required for ' . get_class($this)
+                );
+            }
+        }
+
+        $this->saveCompoundParameterData($compoundData);
+    }
+
+    /**
+     * @return array
+     */
     public function getParameters()
     {
         return $this->parameters;
     }
 
+    /**
+     * @param array $compoundData
+     * @throws \InvalidArgumentException
+     */
     protected function saveCompoundParameterData(array $compoundData)
     {
         foreach ($compoundData as $name => $value) {
