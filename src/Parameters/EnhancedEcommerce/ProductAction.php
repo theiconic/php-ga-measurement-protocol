c4247c2452b4d89e7a8b0a6f4a5610db7cffc481
Jorge A. Borges <contact@jorgeborges.me>
2015-01-29 15:49:29 +1100

    Adding missing docs for ProductAction

--- src/Parameters/EnhancedEcommerce/ProductAction.php
+++ src/Parameters/EnhancedEcommerce/ProductAction.php
@@ -14,42 +14,42 @@ use TheIconic\Tracking\GoogleAnalytics\Parameters\SingleParameter;
 class ProductAction extends SingleParameter
 {
     /**
-     *
+     * Value for product action detail.
      */
     const PRODUCT_ACTION_DETAIL = 'detail';
 
     /**
-     *
+     * Value for product action click.
      */
     const PRODUCT_ACTION_CLICK = 'click';
 
     /**
-     *
+     * Value for product action add.
      */
     const PRODUCT_ACTION_ADD = 'add';
 
     /**
-     *
+     * Value for product action remove.
      */
     const PRODUCT_ACTION_REMOVE = 'remove';
 
     /**
-     *
+     * Value for product action checkout.
      */
     const PRODUCT_ACTION_CHECKOUT = 'checkout';
 
     /**
-     *
+     * Value for product action checkout option.
      */
     const PRODUCT_ACTION_CHECKOUTOPTION = 'checkout_option';
 
     /**
-     *
+     * Value for product action purchase.
      */
     const PRODUCT_ACTION_PURCHASE = 'purchase';
 
     /**
-     *
+     * Value for product action refund.
      */
     const PRODUCT_ACTION_REFUND = 'refund';
 
