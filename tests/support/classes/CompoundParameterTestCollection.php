f3ad43c749f15e4be82ce321594a916e89ffac54
Jorge A. Borges <contact@jorgeborges.me>
2015-01-29 21:17:58 +1100

    Adding unit tests for CompoundParameterCollection

---
+++ tests/support/classes/CompoundParameterTestCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TheIconic\Tracking\GoogleAnalytics\Tests;
+
+use TheIconic\Tracking\GoogleAnalytics\Parameters\CompoundParameterCollection;
+
+class CompoundParameterTestCollection extends CompoundParameterCollection
+{
+    protected $collectionPrefix = 'cp:i:t';
+}
