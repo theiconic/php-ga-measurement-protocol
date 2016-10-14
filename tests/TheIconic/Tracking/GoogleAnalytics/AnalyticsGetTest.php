4f57d5fdf5526f2a1e33b85b969f07aa5bc53081
Jorge A. Borges <contact@jorgeborges.me>
2016-10-14 13:18:48 +1100

    Adding unit tests for Get feature

--- tests/TheIconic/Tracking/GoogleAnalytics/AnalyticsGetTest.php
+++ tests/TheIconic/Tracking/GoogleAnalytics/AnalyticsGetTest.php
@@ -2,28 +2,6 @@
 
 namespace TheIconic\Tracking\GoogleAnalytics;
 
-use TheIconic\Tracking\GoogleAnalytics\Parameters\ContentGrouping\ContentGroup;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\Affiliation;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\CouponCode;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\Product;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\ProductAction;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\ProductCollection;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\Revenue;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\Shipping;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\Tax;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\EnhancedEcommerce\TransactionId;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\General\AnonymizeIp;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\General\CacheBuster;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\General\DataSource;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\General\ProtocolVersion;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\General\QueueTime;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\General\TrackingId;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\Hit\NonInteractionHit;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\Session\IpOverride;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\TrafficSources\GoogleDisplayAdsId;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\User\ClientId;
-use TheIconic\Tracking\GoogleAnalytics\Parameters\Hit\HitType;
-
 /**
  * Class AnalyticsGetTest
  * @package TheIconic\Tracking\GoogleAnalytics
@@ -49,21 +27,21 @@ class AnalyticsGetTest extends \PHPUnit_Framework_TestCase
 
     public function testGetParameter()
     {
-        $Analytics = $this->analytics
+        $analytics = $this->analytics
             ->setProtocolVersion('1')
             ->setTrackingId('UA-26293424-11')
             ->setClientId('555')
             ->setDocumentPath('/');
 
-        $this->assertEquals('1', $Analytics->getProtocolVersion());
-        $this->assertEquals('UA-26293424-11', $Analytics->getTrackingId());
-        $this->assertEquals('555', $Analytics->getClientId());
-        $this->assertEquals('/', $Analytics->getDocumentPath());
+        $this->assertEquals('1', $analytics->getProtocolVersion());
+        $this->assertEquals('UA-26293424-11', $analytics->getTrackingId());
+        $this->assertEquals('555', $analytics->getClientId());
+        $this->assertEquals('/', $analytics->getDocumentPath());
     }
 
     public function testGetIndexedParameter()
     {
-        $Analytics = $this->analytics
+        $analytics = $this->analytics
             ->setProtocolVersion('1')
             ->setTrackingId('UA-26563724-11')
             ->setClientId('555')
@@ -71,25 +49,25 @@ class AnalyticsGetTest extends \PHPUnit_Framework_TestCase
             ->setProductImpressionListName('list name', 1)
             ->setProductImpressionListName('list test', 2);
 
-        $this->assertEquals('1', $Analytics->getProtocolVersion());
-        $this->assertEquals('UA-26563724-11', $Analytics->getTrackingId());
-        $this->assertEquals('555', $Analytics->getClientId());
-        $this->assertEquals('/', $Analytics->getDocumentPath());
-        $this->assertEquals('list name', $Analytics->getProductImpressionListName(1));
-        $this->assertEquals('list test', $Analytics->getProductImpressionListName(2));
-        $this->assertEquals(null, $Analytics->getProductImpressionListName(3));
+        $this->assertEquals('1', $analytics->getProtocolVersion());
+        $this->assertEquals('UA-26563724-11', $analytics->getTrackingId());
+        $this->assertEquals('555', $analytics->getClientId());
+        $this->assertEquals('/', $analytics->getDocumentPath());
+        $this->assertEquals('list name', $analytics->getProductImpressionListName(1));
+        $this->assertEquals('list test', $analytics->getProductImpressionListName(2));
+        $this->assertEquals(null, $analytics->getProductImpressionListName(3));
     }
 
     public function testGetNullParameter()
     {
-        $Analytics = $this->analytics;
+        $analytics = $this->analytics;
 
-        $this->assertEquals(null, $Analytics->getProtocolVersion());
-        $this->assertEquals(null, $Analytics->getTrackingId());
-        $this->assertEquals(null, $Analytics->getClientId());
-        $this->assertEquals(null, $Analytics->getDocumentPath());
-        $this->assertEquals(null, $Analytics->getProductImpressionListName(1));
-        $this->assertEquals(null, $Analytics->getProductImpressionListName(2));
+        $this->assertEquals(null, $analytics->getProtocolVersion());
+        $this->assertEquals(null, $analytics->getTrackingId());
+        $this->assertEquals(null, $analytics->getClientId());
+        $this->assertEquals(null, $analytics->getDocumentPath());
+        $this->assertEquals(null, $analytics->getProductImpressionListName(1));
+        $this->assertEquals(null, $analytics->getProductImpressionListName(2));
     }
 
     /**
@@ -102,49 +80,46 @@ class AnalyticsGetTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException TheIconic\Tracking\GoogleAnalytics\Exception\InvalidIndexException
+     * @expectedException \TheIconic\Tracking\GoogleAnalytics\Exception\InvalidIndexException
      */
     public function testGetInvalidIndexedParameter()
     {
-        $Analytics = $this->analytics
+        $analytics = $this->analytics
             ->setProductImpressionListName('list name', 1)
             ->setProductImpressionListName('list test', 2);
 
-        $Analytics->getProductImpressionListName();
+        $analytics->getProductImpressionListName();
     }
 
     public function testGetProductAction()
     {
-        $Analytics = $this->analytics->setProductActionToCheckoutOption();
-        $this->assertEquals("checkout_option", $Analytics->getProductAction());
+        $analytics = $this->analytics->setProductActionToCheckoutOption();
+        $this->assertEquals("checkout_option", $analytics->getProductAction());
 
-        $Analytics = $this->analytics->setProductActionToPurchase();
-        $this->assertEquals("purchase", $Analytics->getProductAction());
+        $analytics = $this->analytics->setProductActionToPurchase();
+        $this->assertEquals("purchase", $analytics->getProductAction());
 
-        $Analytics = $this->analytics->setProductActionToAdd();
-        $this->assertEquals("add", $Analytics->getProductAction());
+        $analytics = $this->analytics->setProductActionToAdd();
+        $this->assertEquals("add", $analytics->getProductAction());
 
-        $Analytics = $this->analytics->setProductActionToClick();
-        $this->assertEquals("click", $Analytics->getProductAction());
+        $analytics = $this->analytics->setProductActionToClick();
+        $this->assertEquals("click", $analytics->getProductAction());
 
-        $Analytics = $this->analytics->setProductActionToDetail();
-        $this->assertEquals("detail", $Analytics->getProductAction());
+        $analytics = $this->analytics->setProductActionToDetail();
+        $this->assertEquals("detail", $analytics->getProductAction());
 
-        $Analytics = $this->analytics->setProductActionToRefund();
-        $this->assertEquals("refund", $Analytics->getProductAction());
+        $analytics = $this->analytics->setProductActionToRefund();
+        $this->assertEquals("refund", $analytics->getProductAction());
 
-        $Analytics = $this->analytics->setProductActionToRemove();
-        $this->assertEquals("remove", $Analytics->getProductAction());
-
-
+        $analytics = $this->analytics->setProductActionToRemove();
+        $this->assertEquals("remove", $analytics->getProductAction());
     }
 
     public function testGetPromotionAction()
     {
-        $Analytics = $this->analytics->setPromotionActionToClick();
+        $analytics = $this->analytics->setPromotionActionToClick();
 
-        $this->assertEquals("click", $Analytics->getPromotionAction());
-
+        $this->assertEquals("click", $analytics->getPromotionAction());
     }
 
     public function testGetAddItem()
@@ -170,31 +145,20 @@ class AnalyticsGetTest extends \PHPUnit_Framework_TestCase
             'category' => 'Test Category 1/Test Category 2',
             'variant' => 'blue',
             'price' => 85.00,
-            'quantity' => 2,
-            'coupon_code' => 'TEST',
             'position' => 4
         ];
 
-        $response = $this->analytics->addProduct($productData);
+        $this->analytics->setProductImpressionListName('List_1', 1);
 
-        $this->assertEquals('AAAA-6666', $response->getProduct()[0]['sku']);
-        $this->assertEquals('AAAA-5555', $response->getProduct()[1]['sku']);
-        $this->assertEquals('Test Product', $response->getProduct()[1]['name']);
-        $this->assertEquals('Test Brand', $response->getProduct()[1]['brand']);
-        $this->assertEquals('Test Category 1/Test Category 2', $response->getProduct()[1]['category']);
-        $this->assertEquals('blue', $response->getProduct()[1]['variant']);
-        $this->assertEquals(85.00, $response->getProduct()[1]['price']);
-        $this->assertEquals(2, $response->getProduct()[1]['quantity']);
-        $this->assertEquals('TEST', $response->getProduct()[1]['coupon_code']);
-        $this->assertEquals(4, $response->getProduct()[1]['position']);
+        $this->analytics->addProductImpression($productData, 1);
+
+        $this->assertEquals('AAAA-6666', $this->analytics->getProduct()[0]['sku']);
+        $this->assertEquals('AAAA-5555', $this->analytics->getProductImpression(1)[0]['sku']);
+        $this->assertEquals('List_1', $this->analytics->getProductImpressionListName(1));
     }
 
     public function testGetNullItems()
     {
         $this->assertEquals(null, $this->analytics->getProduct());
     }
-
-
-
-
 }
